1235407a2684ae7d0d87fc6fad7ff69f3cf25dda
Kevin Varchola <kevin.keko321@gmail.com>
2024-10-21 21:25:16 +0200

    Edited cluster size and added file info structure

--- structures.go
+++ structures.go
@@ -2,12 +2,13 @@ package main
 
 // Constants for file system
 const (
-	ClusterSize   = 512  // Each cluster is 512 bytes
+	ClusterSize   = 1024 // 1KB cluster size
 	MaxFileName   = 12   // 8.3 format = 11 chars + null terminator
 	MaxClusters   = 1024 // Max number of clusters in the file system
 	FAT_FREE      = -1   // FAT free cluster marker
 	FAT_EOF       = -2   // FAT end of file marker
 	MaxFileLength = 5000 // Maximum file length for the `short` command
+	FAT_ENTRY     = 4    // FAT entry size in bytes
 )
 
 // FAT entry struct to simulate FAT table
@@ -26,4 +27,9 @@ type FileSystem struct {
 	FatTable    FAT
 	Directory   map[string]DirectoryEntry
 	ClusterData [][]byte // Storage for the actual file data
+}
+
+type FileInfo struct {
+	Name string
+	Size int
 }
