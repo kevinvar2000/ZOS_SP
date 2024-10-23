600868ad1c584e73ce6ec476c137a9cdf7da6f0a
Kevin Varchola <kevin.keko321@gmail.com>
2024-10-23 15:24:56 +0200

    Changed const names

--- structures.go
+++ structures.go
@@ -2,17 +2,17 @@ package main
 
 // Constants for file system
 const (
-	ClusterSize   = 1024 // 1KB cluster size
-	MaxFileName   = 12   // 8.3 format = 11 chars + null terminator
-	MaxClusters   = 1024 // Max number of clusters in the file system
-	FAT_FREE      = -1   // FAT free cluster marker
-	FAT_EOF       = -2   // FAT end of file marker
-	MaxFileLength = 5000 // Maximum file length for the `short` command
-	FAT_ENTRY     = 4    // FAT entry size in bytes
+	CLUSTER_SIZE      = 1024 // 1KB cluster size
+	MAX_FILE_NAME     = 12   // 8.3 format = 11 chars + null terminator
+	MAX_CLUSTER_COUNT = 1024 // Max number of clusters in the file system
+	FAT_FREE          = -1   // FAT free cluster marker
+	FAT_EOF           = -2   // FAT end of file marker
+	MAX_FILE_LEN      = 5000 // Maximum file length for the `short` command
+	FAT_ENTRY         = 4    // FAT entry size in bytes
 )
 
 // FAT entry struct to simulate FAT table
-type FAT [MaxClusters]int
+type FAT [MAX_CLUSTER_COUNT]int
 
 // DirectoryEntry stores file metadata
 type DirectoryEntry struct {
